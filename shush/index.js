adc8fe3447cfa2cadd6e053d27c2ede37558d87f
Shi Saaristo <332256+shinmai@users.noreply.github.com>
2025-02-17 15:26:40 +0200

    Create index.js

---
+++ shush/index.js
@@ -0,0 +1,20 @@
+import { findByProps } from "@vendetta/metro"
+import { before } from "@vendetta/patcher"
+
+const shushUpBby = args => {
+  const msg = (args.length>1)?args[1]:args[0].parsedMessage
+  if(!msg.content.startsWith(".")) msg.content = '@silent ' + msg.content
+  else msg.content = msg.content.substring(1)
+}
+let unpatch = []
+
+export default {
+  onLoad() {
+    unpatch.push(before("sendMessage", findByProps("sendMessage", "receiveMessage"), shushUpBby))
+    unpatch.push(before("uploadLocalFiles", findByProps("uploadLocalFiles"), shushUpBby))
+  }
+  onUnload() {
+    this.unpatch.forEach((x) => x())
+		this.unpatch = []
+  }
+}
