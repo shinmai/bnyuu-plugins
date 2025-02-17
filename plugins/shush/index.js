e299d4e09a5f1a423640c7de3c0ff2780febcc7f
Shi Saaristo <332256+shinmai@users.noreply.github.com>
2025-02-17 15:49:40 +0200

    Update index.js

--- plugins/shush/index.js
+++ plugins/shush/index.js
@@ -12,7 +12,7 @@ export default {
   onLoad() {
     unpatch.push(before("sendMessage", findByProps("sendMessage", "receiveMessage"), shushUpBby))
     unpatch.push(before("uploadLocalFiles", findByProps("uploadLocalFiles"), shushUpBby))
-  }
+  },
   onUnload() {
     unpatch.forEach((x) => x())
     unpatch = []
